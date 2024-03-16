5b1882320f74d67d19872f3045c238839887933d
JamesBond <introbond.dev@gmail.com>
2024-03-16 21:21:32 +0700

    Add API key authentication middleware

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,30 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/bondzai/test/data"
 	"github.com/gofiber/fiber/v2"
 )
 
+func APIKeyAuthMiddleware(apiKey string) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		clientAPIKey := c.Get("X-API-Key")
+
+		if clientAPIKey != apiKey {
+			return c.Status(http.StatusUnauthorized).SendString("Unauthorized")
+		}
+
+		return c.Next()
+	}
+}
+
 func main() {
+	apiKey := "your-api-key"
+
 	app := fiber.New()
+
+	app.Use(APIKeyAuthMiddleware(apiKey))
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
