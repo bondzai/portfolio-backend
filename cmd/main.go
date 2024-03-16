4c9ba609d00df26ac4b7a3d6086ce0222f72f171
JamesBond <introbond.dev@gmail.com>
2024-03-16 21:11:05 +0700

    Add URLFilterMiddleware and CORS middleware to main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,26 +1,39 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/bondzai/test/data"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
+// URLFilterMiddleware is a middleware function to filter requests based on requested URL
+func URLFilterMiddleware(allowedURL string) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		requestedURL := c.OriginalURL()
+
+		// Check if the requested URL matches the allowed URL
+		if requestedURL != allowedURL {
+			return c.Status(http.StatusForbidden).SendString("Forbidden")
+		}
+
+		// Allow the request to continue if the requested URL matches the allowed URL
+		return c.Next()
+	}
+}
+
 func main() {
+	allowedURL := "https://thejb.onrender.com"
+
 	app := fiber.New()
 
-	app.Use(func(c *fiber.Ctx) error {
-		allowedOrigin := "https://thejb.onrender.com"
-		origin := c.Get("Origin")
-		if origin == allowedOrigin {
-			c.Set("Access-Control-Allow-Origin", allowedOrigin)
-			c.Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-			c.Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-			if c.Method() == "OPTIONS" {
-				return c.SendStatus(fiber.StatusNoContent)
-			}
-		}
-		return c.Next()
-	})
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: allowedURL,
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
+
+	app.Use(URLFilterMiddleware(allowedURL))
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
