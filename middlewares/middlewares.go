1f02cb8613c92123e9b0a7e2b6d25f80179d0109
JamesBond <introbond.dev@gmail.com>
2024-04-08 10:33:46 +0700

    wip: Refactor

--- middlewares/middlewares.go
+++ middlewares/middlewares.go
@@ -19,26 +19,20 @@ func APIKeyAuthMiddleware(apiKey string) fiber.Handler {
 	}
 }
 
-// AuthTokenMiddleware is a middleware function to authenticate requests using tokens
 func AuthTokenMiddleware(allowedToken string) fiber.Handler {
 	return func(c *fiber.Ctx) error {
-		// Get the Authorization header from the request
 		authHeader := c.Get("Authorization")
 
-		// Check if the Authorization header is missing or doesn't start with "Bearer"
 		if authHeader == "" || !strings.HasPrefix(authHeader, "Bearer ") {
 			return c.Status(http.StatusUnauthorized).SendString("Unauthorized")
 		}
 
-		// Extract the token from the Authorization header
 		token := strings.TrimPrefix(authHeader, "Bearer ")
 
-		// Check if the token matches the allowed token
 		if token != allowedToken {
 			return c.Status(http.StatusUnauthorized).SendString("Unauthorized")
 		}
 
-		// If the token matches, allow the request to proceed
 		return c.Next()
 	}
 }
