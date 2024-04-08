03f75703652109c8d6a8b760eb5410434b338d23
JamesBond <introbond.dev@gmail.com>
2024-04-08 12:48:10 +0700

    Refactor DiscordWebhookService to send IP address in the notification

--- notification/discord.go
+++ notification/discord.go
@@ -1,12 +1,37 @@
 package notification
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"net/http"
+)
 
 type DiscordWebhookService struct {
 	WebhookURL string
 }
 
 func (d *DiscordWebhookService) SendNotification(payload map[string]interface{}) error {
-	fmt.Println("Sending notification to Discord webhook:", payload)
+	data := payload["data"].(map[string]interface{})
+	ip := data["ip"].(string)
+
+	jsonData := fmt.Sprintf(`{"content": "%s"}`, ip)
+
+	req, err := http.NewRequest("POST", d.WebhookURL, bytes.NewBuffer([]byte(jsonData)))
+	if err != nil {
+		log.Printf("Error creating HTTP request: %v", err)
+		return err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		log.Printf("Error sending HTTP request: %v", err)
+		return err
+	}
+
+	defer res.Body.Close()
 	return nil
 }
