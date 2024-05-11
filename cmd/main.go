9387eed7df41a23474baf7c08b201b43c9cbb70f
JamesBond <introbond.dev@gmail.com>
2024-05-11 18:35:52 +0700

    Feat: Insert user to mongo

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"encoding/json"
+	"log"
 	"os"
 	"sync"
+	"time"
 
 	"github.com/bondzai/test/handlers"
+	"github.com/bondzai/test/interfaces"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/websocket/v2"
@@ -24,6 +27,7 @@ type WebSocketManager struct {
 	ActiveUsers int
 	TotalUsers  int
 	mutex       sync.Mutex
+	DBClient    interfaces.MongoDBClientInterface
 }
 
 func (wsm *WebSocketManager) HandleConnection(c *websocket.Conn) {
@@ -33,7 +37,10 @@ func (wsm *WebSocketManager) HandleConnection(c *websocket.Conn) {
 	wsm.ActiveUsers = len(wsm.Connections)
 	wsm.mutex.Unlock()
 
-	wsm.UpdateUserCount()
+	wsm.DBClient.SetDataToMongo(&interfaces.User{
+		Time:       time.Now(),
+		TotalUsers: wsm.TotalUsers,
+	})
 
 	defer func() {
 		wsm.mutex.Lock()
@@ -92,7 +99,14 @@ func main() {
 		AllowCredentials: false,
 	}))
 
-	wsm := &WebSocketManager{}
+	mongoClient, err := interfaces.NewMongoDBClient(os.Getenv("GO_MONGODB_URL"), "JBDB", "JBCOL")
+	if err != nil {
+		log.Print(err)
+	}
+
+	wsm := &WebSocketManager{
+		DBClient: mongoClient,
+	}
 	app.Get("/ws", websocket.New(wsm.HandleConnection))
 
 	handlers.RegisterEndpoints(app)
