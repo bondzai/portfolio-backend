2c88a9a723500957493770880e836617f085d330
JamesBond <introbond.dev@gmail.com>
2024-05-11 18:35:29 +0700

    Initial commit

---
+++ interfaces/influx.go
@@ -0,0 +1,43 @@
+package interfaces
+
+import (
+	"time"
+
+	"github.com/bondzai/test/utils"
+	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
+)
+
+// InfluxClientInterface defines methods required for an InfluxDB client.
+type InfluxClientInterface interface {
+	SetDataToInflux(data *User)
+}
+
+// InfluxClient implements the InfluxClientInterface.
+type InfluxClient struct {
+	client influxdb2.Client
+}
+
+// NewInfluxClient creates a new InfluxClient instance.
+func NewInfluxClient(host, token string) *InfluxClient {
+	return &InfluxClient{
+		client: influxdb2.NewClient(host, token),
+	}
+}
+
+var (
+	org         = utils.GetEnv("GO_INFLUX_ORG", "")
+	user_bucket = utils.GetEnv("GO_INFLUX_BUCKET_USER", "")
+)
+
+// SetDataToInflux writes data to InfluxDB.
+func (c *InfluxClient) SetDataToInflux(data *User) {
+	writeAPI := c.client.WriteAPI(org, user_bucket)
+
+	point := influxdb2.NewPointWithMeasurement("measurement").
+		AddField("usage_count", data.TotalUsers).
+		AddTag("other", "other").
+		SetTime(time.Now())
+
+	writeAPI.WritePoint(point)
+	writeAPI.Flush()
+}
