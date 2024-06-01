8078e5eae1f4343ac94c0a6d00643bcb94528fa4
James Bond <introbond.dev@gmail.com>
2024-06-01 08:25:02 +0700

    Refactor: Websocket endpoint & health check handler

--- cmd/main.go
+++ cmd/main.go
@@ -5,28 +5,18 @@ import (
 
 	"github.com/bondzai/portfolio-backend/config"
 	"github.com/bondzai/portfolio-backend/internal/adapters/handler"
-	repository "github.com/bondzai/portfolio-backend/internal/adapters/repository"
-	usecases "github.com/bondzai/portfolio-backend/internal/core"
+	"github.com/bondzai/portfolio-backend/internal/adapters/repository"
 	"github.com/bondzai/portfolio-backend/internal/core/services"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/websocket/v2"
-
-	"github.com/robfig/cron/v3"
 )
 
 var cfg = config.LoadConfig()
 
 func main() {
 	app := fiber.New()
-
-	app.Use(cors.New(cors.Config{
-		AllowCredentials: false,
-		AllowOrigins:     cfg.CorsOrigin,
-		AllowHeaders:     cfg.CorsHeader,
-		ExposeHeaders:    "Content-Length",
-	}))
 
 	mockRepo := repository.NewMock()
 	mongoReo := initMongoDB()
@@ -36,7 +26,7 @@ func main() {
 	skillService := services.NewSkillService(mockRepo)
 	wakaService := services.NewStatService()
 
-	userManager := usecases.NewManager(mongoReo)
+	wsSerivce := services.NewWsService(mongoReo)
 
 	handler := handler.NewHttpHandler(
 		certService,
@@ -45,15 +35,22 @@ func main() {
 		wakaService,
 	)
 
-	app.Get("/", handler.GetCerts)
+	app.Use(cors.New(cors.Config{
+		AllowCredentials: false,
+		AllowOrigins:     cfg.CorsOrigin,
+		AllowHeaders:     cfg.CorsHeader,
+		ExposeHeaders:    "Content-Length",
+	}))
+
+	app.Get("/", handler.HealthCheck)
 	app.Get("/certifications", handler.GetCerts)
 	app.Get("/projects", handler.GetProjects)
 	app.Get("/skills", handler.GetSkills)
 	app.Get("/wakatime", handler.GetWakaStats)
 
-	app.Get("/ws", websocket.New(userManager.HandleConnection))
+	app.Get("/ws", websocket.New(wsSerivce.HandleConnection))
 
-	startCronJob(userManager)
+	wsSerivce.StartCronJob()
 
 	app.Listen(":" + cfg.Port)
 }
@@ -70,17 +67,4 @@ func initMongoDB() repository.MongoDBClientInterface {
 	}
 
 	return mongoReo
-}
-
-func startCronJob(userManager *usecases.Manager) {
-	c := cron.New()
-
-	c.AddFunc("59 23 * * *", func() {
-		userManager.ResetDailyUserCount()
-	})
-
-	c.Start()
-	defer c.Stop()
-
-	log.Println("cron started...")
 }
