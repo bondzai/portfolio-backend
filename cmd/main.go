1e8f9b2695d468bf3d833eb1d6cd433be30f3ff3
James Bond <introbond.dev@gmail.com>
2024-06-18 20:46:51 +0700

    Refactor

--- cmd/main.go
+++ cmd/main.go
@@ -4,7 +4,7 @@ import (
 	"log/slog"
 
 	"github.com/bondzai/portfolio-backend/config"
-	"github.com/bondzai/portfolio-backend/internal/handler"
+	"github.com/bondzai/portfolio-backend/internal/handlers"
 	"github.com/bondzai/portfolio-backend/internal/repository"
 	"github.com/bondzai/portfolio-backend/internal/usecase"
 	"github.com/gofiber/fiber/v2"
@@ -54,14 +54,14 @@ func main() {
 	wakaService := usecase.NewStatService()
 	websocketService := usecase.NewWsService(mongoRepo)
 
-	restHandler := handler.NewHttpHandler(
+	restHandler := handlers.NewHttpHandler(
 		certService,
 		projectService,
 		skillService,
 		wakaService,
 	)
 
-	websocketHandler := handler.NewWsHandler(websocketService)
+	websocketHandler := handlers.NewWsHandler(websocketService)
 
 	app.Get("/", restHandler.HealthCheck)
 	app.Get("/certifications", restHandler.GetCerts)
--- internal/handlers/http.go
+++ internal/handlers/http.go
@@ -1,4 +1,4 @@
-package handler
+package handlers
 
 import (
 	"github.com/bondzai/portfolio-backend/internal/usecase"
